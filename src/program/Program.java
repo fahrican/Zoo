1eea4e46d48306b6a5f078b8bcc5e83104e68f2a
Fahri MacBook <fahri-can@web.de>
2018-01-22 19:27:37 +0100

    tested tiger in water cage - test successfully false

--- src/program/Program.java
+++ src/program/Program.java
@@ -12,10 +12,16 @@ public class Program {
         Date birthday = new Date(2018, 1, 20);
         Wald wald = new Wald("Auwald", (float)50.5, (float)100.5);
         Tiger tiger = new Tiger(birthday, 'W', "Maya", wald, "Thailand");
+        Tiger tigerNancy = new Tiger(birthday, 'W', "Nancy", wald, "Bangladesch");
         Käfig käfig = new Käfig(1, "Tiger Käfig", (float) 66, (float) 66, (float) 66);
+        WasserTyp salzwasser = WasserTyp.Salzwasser;
+
+        Wasser wasser = new Wasser("Meer", (float) 50, (float) 50, 100, salzwasser);
 
         zoo.addKäfig(käfig);
         System.out.println(zoo.steckeTierInFreienKäfig(tiger, wald));
+        System.out.println(zoo.steckeTierInFreienKäfig(tigerNancy, wasser));
+        System.out.println();
         zoo.listAlleTiere();
 
     }
